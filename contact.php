f6d87c7a798ebf5fb8093fccc16e1d86606f55cb
Verefer <Verefer.Asura@gmail.com>
2024-02-25 13:51:00 +0600

    edit project

---
+++ contact.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Главная страница</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+<?php include 'includes/header.php'; ?>
+
+<div class="content">
+<h1>Контакты</h1>
+            <br><br>
+            <div class="contact">
+                <a href="https://vk.com/verefer">
+                    <img src="assets/images/vk_logo.png" alt="VK Logo">
+                    <p>VK</p>
+                </a>
+            </div>
+            <div class="contact">
+                <a href="https://t.me/verefer">
+                    <img src="assets/images/telegram_logo.png" alt="Telegram Logo">
+                    <p>Telegram</p>
+                </a>
+            </div>
+            <div class="contact">
+                <a href="https://wa.me/message/TCQBCKZE3XGAM1">
+                    <img src="assets/images/whatsapp_logo.png" alt="WhatsApp Logo">
+                    <p>WhatsApp</p>
+                </a>
+            </div>
+            <div class="contact">
+                <a href="https://github.com/verefer">
+                    <img src="assets/images/github_logo.png" alt="GitHub Logo">
+                    <p>GitHub</p>
+                </a>
+            </div>
+</div>
+
+<?php include 'includes/footer.php'; ?>
