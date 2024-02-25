a09e6534f8ec64839603494a8ffac97bc80af9d8
Verefer <Verefer.Asura@gmail.com>
2024-02-25 14:10:05 +0600

    -m

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,5 @@
 <footer>
     <div class="footer-content">
-        <p>&copy; <?php echo date("Y"); ?> Наша Компания. Все права защищены.</p>
-        <ul class="social-links">
-            <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-        </ul>
+        <p>&copy; <?php echo date("Y"); ?> All rights reserved</p>
     </div>
 </footer>
