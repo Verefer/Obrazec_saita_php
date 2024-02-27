cfa4de653b8aeef293e6ee1425e31eb579c06472
Verefer <Verefer.Asura@gmail.com>
2024-02-27 12:23:25 +0600

    modified:   asets/css/styles.css 	modified:   asets/js/script_anim.js

--- asets/js/script_anim.js
+++ asets/js/script_anim.js
@@ -1,29 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-        var sidebar = document.getElementById('sidebar');
-        var sidebarItems = document.querySelectorAll('#sidebar ul li');
-    
+    var sidebar = document.getElementById('sidebar');
+    var sidebarItems = document.querySelectorAll('sidebar ul li');
 
-        // Добавляем анимацию при наведении на пункты меню
-        sidebarItems.forEach(function(item) {
-            item.style.transition = 'background-color 0.3s ease';
+    // Добавляем анимацию при наведении на пункты меню в боковой панели
+    sidebarItems.forEach(function(item) {
+        item.style.transition = 'background-color 0.3s ease';
+    });
+
+    // Добавляем обработчики событий для каждой ссылки в боковой панели
+    sidebarItems.forEach(function(item) {
+        var link = item.querySelector('a');
+        link.style.transition = 'background-color 0.3s ease';
+
+        // Обработчик события для наведения на ссылку
+        link.addEventListener('mouseover', function() {
+            link.style.backgroundColor = '#000'; // Исправлено на черный цвет
+            link.style.color = '#fff'; // Исправлено на белый цвет
         });
-    
-        // Добавляем анимацию при наведении на ссылки в меню
-        sidebarItems.forEach(function(item) {
-            var link = item.querySelector('a'); 
-            link.style.transition = 'background-color 0.3s ease';
-    
-            // Обработчик события для наведения на ссылку
-            link.addEventListener('mouseover', function() {
-                link.style.backgroundColor = '#black';
-                link.style.color = '#fff';
-            });
-    
-            // Обработчик события для ухода курсора с ссылки
-            link.addEventListener('mouseout', function() {
-                link.style.backgroundColor = '#ccc'; 
-                link.style.color = '#000';
-            });
+
+        // Обработчик события для ухода курсора с ссылки
+        link.addEventListener('mouseout', function() {
+            link.style.backgroundColor = 'transparent'; // Исправлено на прозрачный цвет
+            link.style.color = '#000'; // Исправлено на черный цвет
         });
     });
-    
+});
